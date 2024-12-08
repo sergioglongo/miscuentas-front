56e1b1e8cdc93a47b07f63a95f870283e3bc6739
Fernando <fermendoza2494@gmail.com>
2024-12-07 23:59:29 -0300

    vitest initial setup and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 const ReactCompilerConfig = {};
+
+const isTest = process.env.NODE_ENV === "test";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +15,12 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
       },
     }),
+    !isTest && TanStackRouterVite(),
   ],
+  test: {
+    globals: true,
+    isolate: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.tsx",
+  },
 });
