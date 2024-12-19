24a1bcc7c3e737a637a143f7e8ad0c6650569565
Fernando <fermendoza2494@gmail.com>
2024-12-18 21:58:01 -0300

    updating packages

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,8 +17,8 @@ const TanStackRouterDevtools =
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold ">
+      <div className="flex gap-2 p-2">
+        <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>
         <Link to="/about" className="[&.active]:font-bold">
